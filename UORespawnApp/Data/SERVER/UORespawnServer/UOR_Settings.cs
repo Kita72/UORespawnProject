e56de970af41c601b0316de6c1aee9db26c99f29
Tory Bertram <kita.tory@hotmail.com>
2026-03-04 04:49:38 -0700

    Dev Update - Update 2.0.1.0

--- UORespawnApp/Data/SERVER/UORespawnServer/UOR_Settings.cs
+++ UORespawnApp/Data/SERVER/UORespawnServer/UOR_Settings.cs
@@ -5,7 +5,7 @@ namespace Server.Custom.UORespawnServer
 {
     internal static class UOR_Settings
     {
-        internal const string VERSION = "2.0.0.10";
+        internal const string VERSION = "2.0.1.0";
 
         // System Scaler (exposed for ControlService)
         internal static double SCALE_MOD { get; private set; } = 1.0;
